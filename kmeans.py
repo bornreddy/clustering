885d04ee123a8f0aa2f5baefa989b9dd5b031838
bornreddy <marisa.reddy@gmail.com>
2014-06-12 12:04:02 -0400

    kmeans with random centroids working

--- kmeans.py
+++ kmeans.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image
 import random
+import color as cl
 
 #must take in a grayscale image
 def histogramify(image):
@@ -15,32 +16,41 @@ def histogramify(image):
 
 def kmeans(image, k):
   #set up dictionary with random centroids
-  for i in range(0,k):
-    random_centroids = random.sample(range(0,255), k)
-    print random_centroids
-    clusters = {}
-    for c in random_centroids:
-      clusters[c] = []
+  random_centroids = random.sample(range(0,255), k)
+  print random_centroids
+  clusters = {}
+  for c in random_centroids:
+    clusters[c] = []
   #got through pixels and assign to the right clusters as tuples
   distances = []
-  min_distance = 255000000
   for x in range(0,image.size[0]):
     for y in range(0,image.size[1]):
+      min_distance = 250000000
       intensity = image.getpixel((x,y))
+      print intensity
       for c in random_centroids:
         distance = abs(c-intensity)
         if distance < min_distance:
           min_distance = distance
           right_cluster = c
-      print right_cluster
       clusters[right_cluster].append((x,y))
-  print clusters
-
-
+  change_colors(clusters, image)
+ 
+def change_colors(dict, image):
+  image = image.convert("RGB")
+  color_index = 0
+  for x in dict.keys():
+    color = cl.list[color_index]
+    for y in dict[x]:
+      image.putpixel(y, color)
+    color_index += 1
+  image.show()
+    
 img = Image.open("images/tiger.jpg")
 img.load()
 img = img.convert('L')
 kmeans(img, 4)
+
 
 
 
